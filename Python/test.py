6e420f82089108a0eef768bd539307fa63bfc4e8
jaychen <javavwv@163.com>
2018-05-20 16:42:22 +0800

    注释

--- Python/test.py
+++ Python/test.py
@@ -127,7 +127,7 @@ else:
     print("The file "+filename+" has about "+str(num_words)+" words.")
 '''
 
-
+'''
 def count_words(filename):
     """计算文件大致包含多少单词"""
     try:
@@ -144,3 +144,75 @@ def count_words(filename):
 
 
 count_words('alice.txt')
+'''
+
+'''
+print("helloworld")
+'''
+'''
+try:
+    while True:
+        num1 = input("please input number1:\n")
+        num2 = input("please input number2:\n")
+        print("the value is "+str(int(num1)+int(num2)))
+except TypeError:
+        print("input number please")
+except ValueError:
+        print("ValueError")
+'''
+'''
+filename = 'flowers.txt'
+
+try:
+    with open(filename) as cat_file:
+        cat_log = cat_file.readlines()
+    for c in cat_log:
+        print(c)
+except FileNotFoundError:
+    pass
+'''
+
+'''
+统计文件中the的数量
+def count_the(filename):
+    with open(filename) as file_object:
+        file = file_object.read()
+    words = file.split()
+    count = 0
+    for word in words:
+        if word.lower() == 'the':
+            count += 1
+    return count
+
+print(count_the('life_and_adventures.txt'))
+'''
+'''
+import json
+
+numbers = [2,3,5,7,11,13]
+
+filename = 'numbers.json'
+
+with open(filename,'w') as f_obj:
+    json.dump(numbers,f_obj)
+
+
+with open(filename) as f_obj:
+    numbers = json.load(f_obj)
+
+print(numbers)
+
+username = input("What is your name")
+filename = 'username.json'
+with open(filename,'w') as f_obj:
+    json.dump(username,f_obj)
+    print("We'll remember you when you come back,"+username+"!")
+
+with open(filename) as f_obj:
+    username = json.load(f_obj)
+    print("Welcom back,"+username+"!")
+'''
+
+
+
+
